c4a4f0d34cd30c28b39346cd4afb4c2a855089b2
DD0s-JCN13 <jimmylin160@gmail.com>
2017-11-14 14:25:06 +0800

    Number

---
+++ src/com/fju/mid/Numbers.java
@@ -0,0 +1,19 @@
+package com.fju.mid;
+
+
+import java.util.Scanner;
+public class Numbers {
+
+	public static void main(String[] args) {
+		Scanner t = new Scanner(System.in);
+		System.out.print("Please enter a number:");
+		String type = t.nextLine();
+		int num = Integer.parseInt(type);
+		for (int i=1;i<=num;i++){
+			System.out.print(i+ " ");
+			if(i%5 ==4){
+				System.out.print("#"+ " ");
+			}
+		}
+	}
+}
